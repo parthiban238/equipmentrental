d180519f3f15056b9acaadbbeccdf36bb0b35490
parthiban238 <parthiban23082007@gmail.com>
2026-09-22 01:38:14 +0530

    Fix CORS for Render frontend

--- src/main/java/equipmentrental/controller/UserController.java
+++ src/main/java/equipmentrental/controller/UserController.java
@@ -18,7 +18,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequestMapping("/api/users")
 @CrossOrigin(origins = {
         "http://localhost:5500",
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
+        "https://equipmentrental-3.onrender.com"
 })
 public class UserController {
 
